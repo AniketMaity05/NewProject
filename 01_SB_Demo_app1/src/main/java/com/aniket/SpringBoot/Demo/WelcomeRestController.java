e8417a2babf18d1123c89b148e8fb57e10c72655
Aniket Maity <aniketmaity0502@gmail.com>
2026-01-31 23:32:33 +0530

    Simplify greeting in WelcomeRestController
    Removed the greeting message and simplified the return statement.

--- 01_SB_Demo_app1/src/main/java/com/aniket/SpringBoot/Demo/WelcomeRestController.java
+++ 01_SB_Demo_app1/src/main/java/com/aniket/SpringBoot/Demo/WelcomeRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeRestController {
 	@GetMapping("/")
 	public String getMsg() {
-		String s="Good Morning !!!";
-		return s + " Welcome to the Maity Resturent..!!";
+		
+		return  " Welcome to the Maity Resturent..!!";
 	}
 }
